77fdf257d38d94d22ab3c8a9d8ea4b5a738f9452
bhushm <50306448+bhushm@users.noreply.github.com>
2021-02-14 23:33:27 -0500

    Add data collection to cluster function
    Also remove unnessecary comments and reformat code

--- countries.py
+++ countries.py
@@ -8,41 +8,78 @@ RESPONSE_FILENAME = "data/covid-stringency-index.csv"
 
 
 def find_shared_countries():
-    """Find all countries that are shared between the datasets (by ISO Code)."""
+    """Find all countries that are shared between the freedom and response datasets (by ISO code)."""
 
-    # The countries (by ISO Code) that are in the freedom dataset.
+    # The countries (by ISO code) that are in the two datasets.
     freedom_countries = []
-
-    # The countries (by ISO code) that are the in response dataset.
     response_countries = []
 
     with open(FREEDOM_FILENAME) as freedom_file:
 
-        # Read the human freedoms file
         freedom_reader = csv.DictReader(freedom_file)
 
         for row in freedom_reader:
-            if row["year"] == "2017":  # Only use the rows from 2017
-                iso_code = row["ISO_code"]  # The ISO Code of the country
 
-                if not iso_code in freedom_countries:
-                    freedom_countries.append(iso_code)  # Add the ISO Code to the list
+            # Only use the rows from 2017 (see README).
+            if row["year"] == "2017":
+                country = row["ISO_code"]
+
+                if not country in freedom_countries:
+                    freedom_countries.append(country)
 
     with open(RESPONSE_FILENAME) as response_file:
 
-        # Read the COVID response file
         response_reader = csv.DictReader(response_file)
 
         for row in response_reader:
-            iso_code = row["Code"]  # The ISO Code of the country
-            if not iso_code in response_countries:
-                response_countries.append(iso_code)  # Add the ISO Code to the list
+            country = row["Code"]
+
+            if not country in response_countries:
+                response_countries.append(country)
 
     freedom_countries = set(freedom_countries)
     response_countries = set(response_countries)
 
-    # Return a list all of the shared country codes
     return sorted(freedom_countries & response_countries)
+
+
+def cluster_countries():
+    """Cluster the countries based on freedom of expression data."""
+
+    countries = find_shared_countries()
+
+    # The columns in the freedom of expression dataset that will be used.
+    freedom_columns = [
+        "pf_expression_killed",
+        "pf_expression_jailed",
+        "pf_expression_influence",
+        "pf_expression_control",
+        "pf_expression_cable",
+        "pf_expression_newspapers",
+        "pf_expression_internet",
+    ]
+
+    freedom_data = {}
+
+    with open(FREEDOM_FILENAME) as freedom_file:
+
+        freedom_reader = csv.DictReader(freedom_file)
+
+        for row in freedom_reader:
+
+            # Check if the row has the right year and country values
+            if row["year"] == "2017" and row["ISO_code"] in countries:
+
+                country = row["ISO_code"]
+
+                try:
+                    freedom_data[country] = [
+                        float(row[column]) for column in freedom_columns
+                    ]
+
+                # Skip the country if any of the values were blank or otherwise could not be converted to a number.
+                except ValueError:
+                    continue
 
 
 # def compare_response_freedom():
