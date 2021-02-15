3980c15fa596d983efc2859ef2089d649c0aea7f
bhushm <50306448+bhushm@users.noreply.github.com>
2021-02-14 22:42:22 -0500

    Format with black

--- countries.py
+++ countries.py
@@ -22,11 +22,11 @@ def find_shared_countries():
         freedom_reader = csv.DictReader(freedom_file)
 
         for row in freedom_reader:
-            if row['year'] == '2017': # Only use the rows from 2017
-                iso_code = row["ISO_code"] # The ISO Code of the country
+            if row["year"] == "2017":  # Only use the rows from 2017
+                iso_code = row["ISO_code"]  # The ISO Code of the country
 
                 if not iso_code in freedom_countries:
-                    freedom_countries.append(iso_code) # Add the ISO Code to the list
+                    freedom_countries.append(iso_code)  # Add the ISO Code to the list
 
     with open(RESPONSE_FILENAME) as response_file:
 
@@ -34,14 +34,14 @@ def find_shared_countries():
         response_reader = csv.DictReader(response_file)
 
         for row in response_reader:
-            iso_code = row["Code"] # The ISO Code of the country
+            iso_code = row["Code"]  # The ISO Code of the country
             if not iso_code in response_countries:
-                response_countries.append(iso_code) # Add the ISO Code to the list
+                response_countries.append(iso_code)  # Add the ISO Code to the list
 
     freedom_countries = set(freedom_countries)
     response_countries = set(response_countries)
 
-    # Return all of the shared country codes
+    # Return a list all of the shared country codes
     return sorted(freedom_countries & response_countries)
 
 
