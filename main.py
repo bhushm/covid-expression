446788a69d578f4799d4af8d3831e0df09ad13ee
bhushm <50306448+bhushm@users.noreply.github.com>
2021-02-23 13:41:56 -0500

    Add script to run functions

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # Import and run functions from scripts here.
+
+from scripts.countries import (
+    shared_countries,
+)
+
+from scripts.clusters import (
+    freedom_data,
+    country_clusters,
+)
+
+from scripts.averages import (
+    freedom_averages,
+    final_response_averages,
+    seasonal_response_averages,
+)
+
+
+countries = shared_countries()
+
+freedom_data = freedom_data(countries)
+clusters = country_clusters(freedom_data)
+
+print(
+    freedom_averages(clusters),
+    final_response_averages(clusters),
+    seasonal_response_averages(clusters),
+    sep="\n",
+)
