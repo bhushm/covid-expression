1677df9e73a397505e53a4b2d09896b689e825d4
bhushm <50306448+bhushm@users.noreply.github.com>
2021-02-24 14:19:54 -0500

    Create file for plots and linear regression

---
+++ scripts/plots.py
@@ -0,0 +1,6 @@
+def final_response_plots():
+    """Generate plots for the final response values for each cluster."""
+
+
+def seasonal_response_plots():
+    """Generate plots for the seasonal response values for each cluster."""
